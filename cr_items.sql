05aaa61b11e904a035b6b89219f887ad603a566f
spmendiratta <spmendiratta@yahoo.com>
2013-08-18 17:57:58 -0500

    added sql script

---
+++ cr_items.sql
@@ -0,0 +1,5 @@
+create table "Items"(item_id varchar(30), country_of_origin varchar(10), description varchar(40), cost real, image_url varchar(60));
+insert into "Items" values('HP001', 'India', 'Himanchali Topi', 10.99, '?');
+insert into "Items" values('HP002', 'India', 'Kullu Topi', 11.99, '?');
+insert into "Items" values('IS002', 'Turkey', 'Istanabul Topi', 12.99, '?');
+
