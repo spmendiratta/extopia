ec02f32e86f2fd62b5902964edfff5af318b3b36
spmendiratta <spmendiratta@yahoo.com>
2013-08-21 11:25:42 -0500

    postgres template

---
+++ cr_users.sql
@@ -0,0 +1,4 @@
+/*create table "Users"(email varchar(60), name varchar(60), interest varchar(40));*/
+/* Sequelize creates a default table from model - appending id, createdAt, updatedAt */
+insert into "Users" values('sp@yahoo.com', 'Satya', 'India', DEFAULT, current_date, current_date);
+insert into "Users" values('mandy@gmail.com', 'Melissa', 'Peru', DEFAULT, current_date, current_date);
